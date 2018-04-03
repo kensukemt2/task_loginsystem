df7f9f1f5fe5647be4d3b27c65f26d2e6226d01b
kensukemt2 <kensukemt2@gmail.com>
2018-04-04 01:23:52 +0900

    mailer packaging

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,13 @@ Rails.application.routes.draw do
       post :confirm
     end
   end
+
   resources :sessions, only:[:new, :create, :destroy ]
   resources :users, only: [ :new, :create, :show ]
   resources :favorites, only: [:create, :destroy]
+
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
